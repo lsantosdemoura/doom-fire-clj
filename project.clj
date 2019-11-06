87175a63acda345f9848cb135bee02a258663028
Lucas Moura <lsantosdemoura99@gmail.com>
2019-11-06 06:40:29 -0300

    First project and repository configurations.

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject doom-clj "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [seesaw "1.5.0"]
+                 [overtone/at-at "1.2.0"]]
+  :repl-options {:init-ns doom-clj.core})
---
+++ test/hello_seesaw/core_test.clj
@@ -0,0 +1,7 @@
+(ns doom-clj.core-test
+  (:require [clojure.test :refer :all]
+            [doom-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
